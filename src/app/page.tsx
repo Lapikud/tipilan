2ac86b559186705d459d9804f7eef173f3eddcc3
Mark Perman <maperm@taltech.ee>
2025-04-17 21:13:01 +0300

    home page template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import "./globals.css";
+import Header from "@/components/Header";
+import HomeComponent from "@/components/HomeComponent";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header />
+      <HomeComponent />
+    </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+    <header className="h-16 bg-gray-100 flex items-center justify-center border-b border-gray-200">
+        header
+    </header>
+);
+
+export default Header;
