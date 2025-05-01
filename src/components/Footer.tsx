32dcbba2cb407d28db8514effd47e2e47730eda0
TFT <artoreinik@gmail.com>
2025-05-01 11:20:01 +0300

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,20 @@
 import { SiDiscord, SiInstagram, SiFacebook } from "react-icons/si";
+import Image from "next/image";
 
 const Footer = () => (
-    <div className="flex items-center justify-center sm:justify-between flex-col sm:flex-row h-60 px-12">
-        <img src="/tipilan-white.svg" alt="TipiLAN Logo" className="h-16 dark:hidden"/>
-        <img src="/tipilan-dark.svg" alt="TipiLAN Logo" className="h-9 ml-3 not-dark:hidden"/>
+    <div className="flex items-center justify-center sm:justify-between flex-col sm:flex-row px-12 py-16 gap-8">
+        <Image src="/tipilan-white.svg" width={300} height={50} alt="TipiLAN Logo" className="h-16 dark:hidden"/>
+        <Image src="/tipilan-dark.svg" width={300} height={50} alt="TipiLAN Logo" className="h-9 ml-3 not-dark:hidden"/>
         {/* Social media */}
         <div className="flex flex-row">
-            <a href="https://discord.gg/eB7sVqgJ9b" target="_blank" rel="noopener noreferrer">
-                <SiDiscord title="Discord" size={'2em'} className="mx-4 text-[#2A2C3F] dark:text-[#EEE5E5]"/>
+            <a href="https://discord.gg/eB7sVqgJ9b" target="_blank" className="mx-4" rel="noopener noreferrer">
+                <SiDiscord title="Discord" size={'2em'} className="text-[#2A2C3F] dark:text-[#EEE5E5]"/>
             </a>
-            <a href="https://instagram.com/tipilan.ee" target="_blank" rel="noopener noreferrer">
-                <SiInstagram title="Instagram" size={'2em'} className="mx-4 text-[#2A2C3F] dark:text-[#EEE5E5]"/>
+            <a href="https://instagram.com/tipilan.ee" target="_blank" className="mx-4" rel="noopener noreferrer">
+                <SiInstagram title="Instagram" size={'2em'} className="text-[#2A2C3F] dark:text-[#EEE5E5]"/>
             </a>
-            <a href="https://facebook.com/tipilan.ee" target="_blank" rel="noopener noreferrer">
-                <SiFacebook title="Facebook" size={'2em'} className="mx-4 text-[#2A2C3F] dark:text-[#EEE5E5]"/>
+            <a href="https://facebook.com/tipilan.ee" target="_blank" className="mx-4" rel="noopener noreferrer">
+                <SiFacebook title="Facebook" size={'2em'} className="text-[#2A2C3F] dark:text-[#EEE5E5]"/>
             </a>
         </div>
     </div>
