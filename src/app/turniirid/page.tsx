df28c516d2b4459dd0bf37da73b64de126627da3
Kerdo Käärma <kerdo.kaarma@gmail.com>
2025-07-24 22:52:41 +0300

    Parandatud kirjaviga

--- src/app/turniirid/page.tsx
+++ src/app/turniirid/page.tsx
@@ -4,7 +4,7 @@ export default function Tourney() {
     return (
         <div className="flex flex-col min-h-[90vh] p-12 pt-18">
             <h1 className={`text-5xl sm:text-6xl ${vipnagorgialla.className} font-bold italic uppercase text-[#2A2C3F] dark:text-[#EEE5E5] mt-8 mb-4`}>Turniirid</h1>
-            <p className="text-2xl text-[#2A2C3F] dark:text-[#EEE5E5]">Kui tahate oma oskusi proovile panna siis vaadake siia tagasi! Rohkem infot lähiajal.</p>
+            <p className="text-2xl text-[#2A2C3F] dark:text-[#EEE5E5]">Kui tahate oma oskusi proovile panna, siis vaadake siia tagasi! Rohkem infot lähiajal.</p>
         </div>
     );
 }
