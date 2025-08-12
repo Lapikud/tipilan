e6bd09616f1ff2546889327210ec07c77c2e2754
SwagMuffin88 <katrilotamois@outlook.com>
2025-08-12 11:17:11 +0300

    fix image skew on tourn page

--- src/app/turniirid/page.tsx
+++ src/app/turniirid/page.tsx
@@ -19,6 +19,7 @@ export default function Tourney() {
             {/*  Kui tahate oma oskusi proovile panna, siis vaadake siia tagasi! Rohkem*/}
             {/*  infot lähiajal.*/}
             {/*</p>*/}
+            
             <div className="flex flex-col gap-8 md:gap-16">
                 {/* CS2 turniir */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 items-center mx-8 md:mx-16 lg:mx-32 xl:mx-48">
@@ -59,8 +60,8 @@ export default function Tourney() {
                         </div>
                     </div>
                     <div className="hidden md:block">
-                        <div className="-skew-x-2 md:-skew-x-5">
-                            {/* Image needs to be the div that has the skew. Outside div needs to remain so that overflow wont occur*/}
+                        <div>
+                            {/* Outside div needs to remain so that overflow won't occur*/}
                             <Image
                                 src="/images/cs2_tournament_logo.png"
                                 alt="CS2 tournament"
@@ -76,8 +77,8 @@ export default function Tourney() {
                 {/* LoL turniir */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 items-center mx-8 md:mx-16 lg:mx-32 xl:mx-48">
                     <div className="hidden md:block">
-                        <div className="-skew-x-2 md:-skew-x-5">
-                            {/* Image needs to be the div that has the skew. Outside div needs to remain so that overflow wont occur*/}
+                        <div>
+                            {/* Outside div needs to remain so that overflow won't occur*/}
                             <Image
                                 src="/images/lol_tournament_logo.png"
                                 alt="LoL tournament"
@@ -161,8 +162,8 @@ export default function Tourney() {
                         </div>
                     </div>
                     <div className="hidden md:block">
-                        <div className="-skew-x-2 md:-skew-x-5">
-                            {/* Image needs to be the div that has the skew. Outside div needs to remain so that overflow wont occur*/}
+                        <div>
+                            {/* Outside div needs to remain so that overflow won't occur*/}
                             <Image
                                 src="/images/minitournament_logo.png"
                                 alt="mini tournaments"
