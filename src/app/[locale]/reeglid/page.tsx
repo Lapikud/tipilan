9906671cc0f11bf1e95f325e9116698bcccf14c4
SwagMuffin88 <katrilotamois@outlook.com>
2025-08-21 17:38:12 +0300

    fix variables in translation files

--- src/app/[locale]/reeglid/page.tsx
+++ src/app/[locale]/reeglid/page.tsx
@@ -9,16 +9,17 @@ export default async function RulesMenu({
   params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
+  
   setRequestLocale(locale);
+  
   const t = await getTranslations({ locale });
+  
   const headingStyle = `text-4xl md:text-5xl lg:text-6xl ${vipnagorgialla.className} font-bold italic text-[#2A2C3F] dark:text-[#EEE5E5] uppercase`;
 
   const boxStyle = `-skew-x-2 md:-skew-x-5 text-white md:px-12 hover:scale-103 transition-all duration-150 w-full md:w-xl lg:w-[400px]`;
 
   const boxTextStyle = `text-3xl ${vipnagorgialla.className} font-bold uppercase text-[#EEE5E5] pb-2`;
-
-  // const SectionDivider = () => <div className="border-b-[3px] border-[#1F5673] w-full"/>;
-
+  
   return (
     <div>
       <div className="flex flex-col md:m-16">
@@ -49,7 +50,7 @@ export default async function RulesMenu({
           {/*<Link href="">*/}
           <div className={`${boxStyle} bg-[#1F5673] py-16 px-8`}>
             <h2 className={`${boxTextStyle}`}>
-              {t("tournaments.mini.titleSingular")} {t("rules.title")}
+              {t("rules.miniRules")} 
             </h2>
           </div>
           {/*</Link>*/}
