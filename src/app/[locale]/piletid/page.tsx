5f498d0fc716250301585bcbc116ab6a2552d256
SwagMuffin88 <katrilotamois@outlook.com>
2025-09-23 09:20:39 +0300

    update ticket prices and availability

--- src/app/[locale]/piletid/page.tsx
+++ src/app/[locale]/piletid/page.tsx
@@ -19,12 +19,13 @@ export default async function Tickets({
         >
           {t("tickets.title")}
         </h1>
+        
         <div className="flex justify-center lg:items-center flex-col lg:flex-row gap-8 md:gap-12 flex-grow mb-16 md:mt-8 lg:mt-0">
           <div className="bg-[#007CAB] -skew-x-2 md:-skew-x-5 text-white px-8 md:px-12 py-16 hover:scale-103 transition-all duration-150 w-full md:w-xl lg:w-[400px]">
             <h2
               className={`text-6xl ${vipnagorgialla.className} font-bold italic text-[#EEE5E5] pb-2`}
             >
-              {t("tickets.computerParticipant.price")}
+              {t("tickets.computerParticipant.latePrice")}
             </h2>
             <h3
               className={`text-3xl ${vipnagorgialla.className} font-bold italic text-[#EEE5E5] pb-4`}
@@ -48,40 +49,41 @@ export default async function Tickets({
               </button>
             </Link>
           </div>
-          <div className="bg-[#1F5673] -skew-x-2 md:-skew-x-5 text-white px-8 md:px-12 py-16 hover:scale-103 transition-all duration-150 w-full md:w-xl lg:w-[400px]">
+          
+          <div className="bg-[#1F5673] -skew-x-2 md:-skew-x-5 text-gray-400 px-8 md:px-12 py-16  w-full md:w-xl lg:w-[400px]">
             <h2
-              className={`text-6xl ${vipnagorgialla.className} font-bold italic text-[#EEE5E5] pb-2`}
+              className={`text-4xl ${vipnagorgialla.className} font-bold italic pb-2`}
             >
-              {t("tickets.competitor.price")}
+              <s>{t("tickets.competitor.price")}</s>
             </h2>
             <h3
-              className={`text-3xl ${vipnagorgialla.className} font-bold italic text-[#EEE5E5] pb-4`}
+              className={`text-2xl ${vipnagorgialla.className} font-bold italic pb-4`}
             >
-              {t("tickets.competitor.title")}
+              <s>{t("tickets.competitor.title")}</s>
             </h3>
             <ul className="pl-4 mb-8 list-[square] marker:text-[#007CAB]">
               {t
                 .raw("tickets.competitor.features")
                 .map((feature: string, index: number) => (
-                  <li key={index} className="text-xl">
+                  <li key={index} className="text-sm">
                     {feature}
                   </li>
                 ))}
             </ul>
-            <Link href="https://fienta.com/et/tipilan" target="_blank">
+            {/*<Link href="https://fienta.com/et/tipilan" target="_blank">*/}
               <button
-                className={`px-4 py-2 bg-[#007CAB] cursor-pointer ${vipnagorgialla.className} font-bold italic`}
+                className={`px-4 py-2 bg-[#007CAB] text-white ${vipnagorgialla.className} font-bold text-xl italic uppercase`}
               >
-                {t("tickets.buyTicket")}
+                {t("tickets.soldOut")}!
               </button>
-            </Link>
+            {/*</Link>*/}
           </div>
 
           <div className="bg-[#007CAB] -skew-x-2 md:-skew-x-5 text-white px-8 md:px-12 py-16 hover:scale-103 transition-all duration-150 w-full md:w-xl lg:w-[400px]">
             <h2
               className={`text-6xl ${vipnagorgialla.className} font-bold italic text-[#EEE5E5] pb-2`}
             >
-              {t("tickets.visitor.price")}
+              {t("tickets.visitor.latePrice")}
             </h2>
             <h3
               className={`text-3xl ${vipnagorgialla.className} font-bold italic text-[#EEE5E5] pb-4`}
