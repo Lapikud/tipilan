ef6812f2d90b9b4ab714ce1b501d5ec98878d5a0
Rene Arumetsa <rene.arumetsa@gmail.com>
2025-10-05 16:11:33 +0300

    Change timetable to be up to date.

--- src/data/timetable.ts
+++ src/data/timetable.ts
@@ -13,19 +13,19 @@ export const scheduleData: Record<string, ScheduleItem[]> = {
       time: "17:00",
     },
     {
-      titleKey: "schedule.events.mainTournamentsStart",
-      locationKey: "schedule.locations.auditorium",
-      time: "20:00",
-    },
-    {
-      titleKey: "schedule.events.miniTournamentsKickoff",
-      locationKey: "schedule.locations.studentHouse",
+      titleKey: "schedule.events.preTournamentWarmup",
+      locationKey: "schedule.locations.auditoriumAndStudentHouse",
       time: "18:00",
     },
     {
-      titleKey: "schedule.events.fightingGamesStart",
+      titleKey: "schedule.events.killerInstinct",
       locationKey: "schedule.locations.studentHouse",
       time: "18:30",
+    },
+    {
+      titleKey: "schedule.events.mainTournamentsStart",
+      locationKey: "schedule.locations.auditorium",
+      time: "20:00",
     },
     {
       titleKey: "schedule.events.doorsClose",
@@ -40,22 +40,27 @@ export const scheduleData: Record<string, ScheduleItem[]> = {
       time: "10:00",
     },
     {
-      titleKey: "schedule.events.miniTournamentsStart",
-      locationKey: "schedule.locations.studentHouse",
-      time: "11:00",
+      titleKey: "schedule.events.warmup",
+      locationKey: "schedule.locations.auditoriumAndStudentHouse",
+      time: "10:30",
     },
     {
-      titleKey: "schedule.events.granblue",
-      locationKey: "schedule.locations.studentHouse",
-      time: "11:30",
-    },
-    {
-      titleKey: "schedule.events.mainTournamentsStart",
+      titleKey: "schedule.events.mainTournamentsContinue",
       locationKey: "schedule.locations.auditorium",
       time: "12:00",
     },
     {
-      titleKey: "schedule.events.granTurismo",
+      titleKey: "schedule.events.miniTournamentsStart",
+      locationKey: "schedule.locations.studentHouse",
+      time: "13:00",
+    },
+    {
+      titleKey: "schedule.events.granblue",
+      locationKey: "schedule.locations.studentHouse",
+      time: "13:30",
+    },
+    {
+      titleKey: "schedule.events.granTurismoFinal",
       locationKey: "schedule.locations.studentHouse",
       time: "20:00",
     },
@@ -63,6 +68,28 @@ export const scheduleData: Record<string, ScheduleItem[]> = {
       titleKey: "schedule.events.doorsClose",
       locationKey: "schedule.locations.auditoriumAndStudentHouse",
       time: "*01:00",
+    },
+  ],
+  oct26: [
+    {
+      titleKey: "schedule.events.expoClosed",
+      locationKey: "schedule.locations.studentHouse",
+      time: "all day",
+    },
+    {
+      titleKey: "schedule.events.cs2FinalStart",
+      locationKey: "schedule.locations.auditorium",
+      time: "10:00",
+    },
+    {
+      titleKey: "schedule.events.cs2Final",
+      locationKey: "schedule.locations.auditorium",
+      time: "18:00",
+    },
+    {
+      titleKey: "schedule.events.awardCeremony",
+      locationKey: "schedule.locations.auditorium",
+      time: "18:30",
     },
   ],
 };
