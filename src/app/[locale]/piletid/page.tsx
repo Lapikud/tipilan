09c6081f7bea744f3870ae7e1671a2dd55472ec8
SwagMuffin88 <katrilotamois@outlook.com>
2025-10-13 13:47:13 +0300

    Change order of ticket options, fix wording

--- src/app/[locale]/piletid/page.tsx
+++ src/app/[locale]/piletid/page.tsx
@@ -23,6 +23,35 @@ export default async function Tickets({
         <div className="flex justify-center lg:items-center flex-col lg:flex-row gap-8 md:gap-12 flex-grow mb-16 md:mt-8 lg:mt-0">
           <div className="bg-[#007CAB] -skew-x-2 md:-skew-x-5 text-white italic px-8 md:px-12 py-16 hover:scale-103 transition-all duration-150 w-full md:w-xl lg:w-[400px]">
             <h2
+                className={`text-6xl ${vipnagorgialla.className} font-bold text-[#EEE5E5] pb-2`}
+            >
+              {t("tickets.visitor.latePrice")}
+            </h2>
+            <h3
+                className={`text-3xl ${vipnagorgialla.className} font-bold text-[#EEE5E5] pb-4`}
+            >
+              {t("tickets.visitor.title")}
+            </h3>
+            <ul className="pl-4 mb-8 list-[square] marker:text-[#1F5673]">
+              {t
+                  .raw("tickets.visitor.features")
+                  .map((feature: string, index: number) => (
+                      <li key={index} className="text-xl">
+                        {feature}
+                      </li>
+                  ))}
+            </ul>
+            <Link href="https://fienta.com/et/tipilan" target="_blank">
+              <button
+                  className={`px-4 py-2 bg-[#1F5673] cursor-pointer ${vipnagorgialla.className} font-bold`}
+              >
+                {t("tickets.buyTicket")}
+              </button>
+            </Link>
+          </div>
+          
+          <div className="bg-[#007CAB] -skew-x-2 md:-skew-x-5 text-white italic px-8 md:px-12 py-16 hover:scale-103 transition-all duration-150 w-full md:w-xl lg:w-[400px]">
+            <h2
               className={`text-6xl ${vipnagorgialla.className} font-bold  text-[#EEE5E5] pb-2`}
             >
               {t("tickets.computerParticipant.latePrice")}
@@ -49,63 +78,36 @@ export default async function Tickets({
               </button>
             </Link>
           </div>
-          
+
+         
+
           <div className="bg-[#1F5673] -skew-x-2 md:-skew-x-5 text-gray-400 italic px-8 md:px-12 py-16  w-full md:w-xl lg:w-[400px]">
             <h2
-              className={`text-4xl ${vipnagorgialla.className} font-bold pb-2`}
+                className={`text-4xl ${vipnagorgialla.className} font-bold pb-2`}
             >
               <s>{t("tickets.competitor.price")}</s>
             </h2>
             <h3
-              className={`text-2xl ${vipnagorgialla.className} font-bold pb-4`}
+                className={`text-2xl ${vipnagorgialla.className} font-bold pb-4`}
             >
               <s>{t("tickets.competitor.title")}</s>
             </h3>
             <ul className="pl-4 mb-8 list-[square] marker:text-[#007CAB]">
               {t
-                .raw("tickets.competitor.features")
-                .map((feature: string, index: number) => (
-                  <li key={index} className="text-sm">
-                    {feature}
-                  </li>
-                ))}
+                  .raw("tickets.competitor.features")
+                  .map((feature: string, index: number) => (
+                      <li key={index} className="text-sm">
+                        {feature}
+                      </li>
+                  ))}
             </ul>
             {/*<Link href="https://fienta.com/et/tipilan" target="_blank">*/}
-              <button
+            <button
                 className={`px-4 py-2 bg-[#007CAB] text-white ${vipnagorgialla.className} font-bold text-xl uppercase opacity-55`}
-              >
-                {t("tickets.soldOut")}!
-              </button>
+            >
+              {t("tickets.soldOut")}!
+            </button>
             {/*</Link>*/}
-          </div>
-
-          <div className="bg-[#007CAB] -skew-x-2 md:-skew-x-5 text-white italic px-8 md:px-12 py-16 hover:scale-103 transition-all duration-150 w-full md:w-xl lg:w-[400px]">
-            <h2
-              className={`text-6xl ${vipnagorgialla.className} font-bold text-[#EEE5E5] pb-2`}
-            >
-              {t("tickets.visitor.latePrice")}
-            </h2>
-            <h3
-              className={`text-3xl ${vipnagorgialla.className} font-bold text-[#EEE5E5] pb-4`}
-            >
-              {t("tickets.visitor.title")}
-            </h3>
-            <ul className="pl-4 mb-8 list-[square] marker:text-[#1F5673]">
-              {t
-                .raw("tickets.visitor.features")
-                .map((feature: string, index: number) => (
-                  <li key={index} className="text-xl">
-                    {feature}
-                  </li>
-                ))}
-            </ul>
-            <Link href="https://fienta.com/et/tipilan" target="_blank">
-              <button
-                className={`px-4 py-2 bg-[#1F5673] cursor-pointer ${vipnagorgialla.className} font-bold`}
-              >
-                {t("tickets.buyTicket")}
-              </button>
-            </Link>
           </div>
         </div>
       </div>
