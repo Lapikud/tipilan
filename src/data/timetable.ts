b8d4d85e0ee6bd997f5e590d45801bf0aaa8ec3c
Rene Arumetsa <rene.arumetsa@gmail.com>
2025-10-19 21:23:48 +0300

    Feature: Fix the timetable..

--- src/data/timetable.ts
+++ src/data/timetable.ts
@@ -12,18 +12,9 @@ export const scheduleData: Record<string, ScheduleItem[]> = {
       locationKey: "schedule.locations.registrationSetup",
       time: "17:00",
     },
+
     {
       titleKey: "schedule.events.miniTournaments",
-      locationKey: "schedule.locations.studentHouse",
-      time: "18:00",
-    },
-    {
-      titleKey: "schedule.events.tournamentRegistration",
-      locationKey: "schedule.locations.auditorium",
-      time: "18:00",
-    },
-    {
-      titleKey: "schedule.events.streetFighter6",
       locationKey: "schedule.locations.studentHouse",
       time: "18:00",
     },
@@ -33,19 +24,9 @@ export const scheduleData: Record<string, ScheduleItem[]> = {
       time: "18:30",
     },
     {
-      titleKey: "schedule.events.cs2Tournament",
+      titleKey: "schedule.events.cs2LolTournaments",
       locationKey: "schedule.locations.auditorium",
       time: "20:00",
-    },
-    {
-      titleKey: "schedule.events.lolTournament",
-      locationKey: "schedule.locations.auditorium",
-      time: "20:00",
-    },
-    {
-      titleKey: "schedule.events.sf6SsbuFinale",
-      locationKey: "schedule.locations.studentHouse",
-      time: "22:00",
     },
     {
       titleKey: "schedule.events.doorsClose",
@@ -55,9 +36,9 @@ export const scheduleData: Record<string, ScheduleItem[]> = {
   ],
   oct25: [
     {
-      titleKey: "schedule.events.doorsOpen",
-      locationKey: "schedule.locations.auditoriumAndStudentHouse",
-      time: "10:00",
+      titleKey: "schedule.events.doorsOpenSimple",
+      locationKey: "schedule.locations.auditorium",
+      time: "09:30",
     },
     {
       titleKey: "schedule.events.cs2Continue",
@@ -70,34 +51,34 @@ export const scheduleData: Record<string, ScheduleItem[]> = {
       time: "12:00",
     },
     {
-      titleKey: "schedule.events.2xkoTournament",
-      locationKey: "schedule.locations.studentHouse",
+      titleKey: "schedule.events.expoOpens",
+      locationKey: "schedule.locations.entranceHall",
       time: "12:00",
     },
     {
-      titleKey: "schedule.events.tekken8Tournament",
+      titleKey: "schedule.events.2xkoTournament",
       locationKey: "schedule.locations.studentHouse",
-      time: "12:30",
-    },
-    {
-      titleKey: "schedule.events.2xkoTekken8Finale",
-      locationKey: "schedule.locations.studentHouse",
-      time: "18:00",
+      time: "13:30",
     },
     {
       titleKey: "schedule.events.lolFinale",
       locationKey: "schedule.locations.auditorium",
-      time: "18:30",
+      time: "19:30",
+    },
+    {
+      titleKey: "schedule.events.cs2lbsemifinals",
+      locationKey: "schedule.locations.auditorium",
+      time: "19:30",
+    },
+    {
+      titleKey: "schedule.events.cs2lbfinals",
+      locationKey: "schedule.locations.auditorium",
+      time: "21:30",
     },
     {
       titleKey: "schedule.events.granTurismoFinale",
       locationKey: "schedule.locations.studentHouse",
       time: "20:00",
-    },
-    {
-      titleKey: "schedule.events.lolTournamentSemifinals",
-      locationKey: "schedule.locations.auditorium",
-      time: "*01:00",
     },
     {
       titleKey: "schedule.events.doorsClose",
@@ -107,24 +88,25 @@ export const scheduleData: Record<string, ScheduleItem[]> = {
   ],
   oct26: [
     {
-      titleKey: "schedule.events.cs2Finale",
+      titleKey: "schedule.events.cs2semifinals",
       locationKey: "schedule.locations.auditorium",
-      time: "10:00",
+      time: "11:00",
     },
     {
-      titleKey: "schedule.events.cs2FinalMatch",
+      titleKey: "schedule.events.Cs2finals",
       locationKey: "schedule.locations.auditorium",
       time: "15:30",
     },
     {
-      titleKey: "schedule.events.cs2TournamentFinale",
+
+      titleKey: "schedule.events.AwardsCeremony",
       locationKey: "schedule.locations.auditorium",
       time: "18:00",
     },
     {
       titleKey: "schedule.events.doorsClose",
       locationKey: "schedule.locations.auditorium",
-      time: "18:30",
+      time: "19:00",
     },
   ],
 };
