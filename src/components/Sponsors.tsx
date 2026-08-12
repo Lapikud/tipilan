c32577062b61acd2cdfe8f704e3ca32110b15ba6
Rene Arumetsa <rene.arumetsa@gmail.com>
2026-08-12 09:36:29 +0300

    Added new sponsor logos

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -86,6 +86,34 @@ const sponsors: Sponsor[] = [
     alt: "iNGAME",
     width: 200,
     height: 200,
+  },
+  {
+    href: "https://www.tallinn.ee/et/haridusamet",
+    src: "/sponsors/Tallinna_Haridusamet_logo_RGB.svg",
+    alt: "Tallinna Haridusamet",
+    width: 200,
+    height: 200,
+  },
+  {
+    href: "https://parkminigolf.ee/",
+    src: "/sponsors/park_minigolf.png",
+    alt: "Park Minigolf",
+    width: 200,
+    height: 200,
+  },
+  {
+    href: "https://www.lemongym.ee/",
+    src: "/sponsors/lemongym.png",
+    alt: "Lemongym",
+    width: 200,
+    height: 200,
+  },
+  {
+    href: "https://www.elamusgolf.eu/",
+    src: "/sponsors/elamusgolf.png",
+    alt: "Elamusgolf",
+    width: 200,
+    height: 200,  
   }
 ];
 
